34677b82f6b497f6adf11224ef6c7f677a67ba07
John Detlefs <jdetlefs@godaddy.com>
2022-05-06 13:24:53 -0500

    Save wokr

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import TrackCard from '../containers/track-card';
-import { Layout, QueryResult } from '../components';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import { Layout, QueryResult } from "../components";
 
 /** TRACKS gql query to retreive all tracks */
 export const TRACKS = gql`
